089a96e9eda64284bfd20894d16daf2e418004b6
Nassredean Nasseri <dnasseri10@gmail.com>
2016-07-24 15:03:12 -0400

    set up factory girl, devise testing, and add a spec for the users controller index and show actions

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe Api::V1::UsersController do
+
+  describe 'GET #show' do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+      auth_headers = @user.create_new_auth_token
+      request.headers.merge!(auth_headers)
+      get :show, id: @user.id
+    end
+
+    it 'responds with 200 status code' do
+      expect(response.code).to eq('200')
+    end
+
+    it 'returns the serialized user attributes' do
+      expect(JSON.parse(response.body)['data']['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    end
+
+  end
+
+  describe 'GET #index' do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+      auth_headers = @user.create_new_auth_token
+      request.headers.merge!(auth_headers)
+      get :index, id: @user.id
+    end
+
+    it 'responds with 200 status code' do
+      expect(response.code).to eq('200')
+    end
+
+    it 'returns the serialized user attributes' do
+      expect(JSON.parse(response.body)['data'].length).to eq(1)
+      expect(JSON.parse(response.body)['data'].first['attributes']).to eq({'name'=>'John Doe', 'email'=>'test@test.com'})
+    end
+
+  end
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name "John Doe"
+    email "test@test.com"
+    password "testpass1234"
+  end
+end

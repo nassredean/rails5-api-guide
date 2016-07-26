4948066e840efd76c5683ef189d528a1bc5ee8d2
Nassredean Nasseri <dnasseri10@gmail.com>
2016-07-25 22:26:02 -0400

    change default devise mail adress

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,4 +2,5 @@ Devise.setup do |config|
   # Using rails-api, tell devise to not use ActionDispatch::Flash
   # middleware b/c rails-api does not include it.
   config.navigational_formats = [:json]
+  config.mailer_sender = "support@myapp.com"
 end

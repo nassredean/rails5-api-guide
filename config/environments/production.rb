6eceed79db4d1cf0b196d19a24b3ed6d357a6f22
Nassredean Nasseri <dnasseri10@gmail.com>
2016-09-14 12:13:12 -0400

    make name more generic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,6 @@ Rails.application.configure do
   config.cache_store = :redis_store, ENV['REDIS_URL']
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "devboard_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
